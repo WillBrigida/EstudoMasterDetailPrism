0987c8c1cc6ca58fc7c479c29e1f1685b0108963
will brigida <will_brigida@hotmail.com>
2019-05-01 16:09:46 -0300

    Lista , Styles e Fontes

---
+++ EstudoMasterDetail/EstudoMasterDetail/Behaviors/MenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstudoMasterDetail.Behaviors
+{
+    public class MenuItem
+    {
+        public string Icone { get; set; }
+        public string Titulo { get; set; }
+        public string Cor { get; set; }
+
+        public MenuItem(string icone, string titulo, string cor)
+        {
+            this.Icone = icone;
+            this.Titulo = titulo;
+            this.Cor = cor;
+        }
+    }
+}
--- EstudoMasterDetail/EstudoMasterDetail/ViewModels/MenuViewModel.cs
+++ EstudoMasterDetail/EstudoMasterDetail/ViewModels/MenuViewModel.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using EstudoMasterDetail.Behaviors;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace EstudoMasterDetail.ViewModels
 {
     public class MenuViewModel
     {
+        public ObservableCollection<MenuItem> ListaMenu { get; set; }
+
+
+        public MenuViewModel()
+        {
+            ListaMenu = new ObservableCollection<MenuItem>
+            {
+                new MenuItem (FontAwesome.Building, "Portaria", "#49a4aa"),
+                new MenuItem (FontAwesome.Briefcase,  "Reparos / Obras", "#e1c27f"),
+                new MenuItem (FontAwesome.Calculator,  "Balancetes", "#000000"),
+            };
+        }
     }
 }
